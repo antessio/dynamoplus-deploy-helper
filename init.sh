63f2e704e4e6aa2b28b23c1d60c1f8e5773a6845
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-02-05 23:11:55 +0100

    fixing init

--- init.sh
+++ init.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-if [ ! -d "dynamoplus" ]
-then
-    echo "Downloading dynamolpus"
-    git clone https://github.com/antessio/dynamoplus.git dynamoplus
-    git checkout develop
-fi
-echo "Create secrets"
-cd dynamoplus/serverless 
-cp secrets-example.json secrets.json
 echo "Building the dynamoplus serverless image"
 docker build . -t dynamoplus-serverless
