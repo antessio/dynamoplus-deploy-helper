c60b522d369f0ee3806761ca59a98e92ee76a41b
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-02-13 23:35:24 +0100

    fixed scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -27,18 +27,20 @@ if [[ -z "${DYNAMODB_PORT}" ]]; then
 fi
 
 echo "service name: $DYNAMOPLUS_SERVICE_NAME"
+echo "dynamodb  host: $DYNAMODB_HOST"
+#cat serverless.yml
 
 sed "s/passwordtochangeinproduction/$DYNAMOPLUS_ROOT_PASSWORD/g" secrets-example.json > secrets.json
-sed -i '' "s/service: dynamoplus/service: $DYNAMOPLUS_SERVICE_NAME/g" serverless.yml
-sed -i '' 's/dockerizePip: true/dockerizePip: false/g' serverless.yml
-sed -E -i '' "s/(DYNAMODB_HOST:).*/\1 $DYNAMODB_HOST/g" serverless.yml
-sed -E -i '' "s/(DYNAMODB_PORT:).*/\1 $DYNAMODB_PORT/g" serverless.yml 
+sed -i "s/service: dynamoplus/service: $DYNAMOPLUS_SERVICE_NAME/g" serverless.yml
+sed -i 's/dockerizePip: true/dockerizePip: false/g' serverless.yml
+sed -i -E "s@(DYNAMODB_HOST:).*@\1 $DYNAMODB_HOST@" serverless.yml
+sed -i -E "s@(DYNAMODB_PORT:).*@\1 $DYNAMODB_PORT@" serverless.yml 
 
-echo "-----------"
-echo "secrets"
+# echo "-----------"
+# echo "secrets"
 
-cat secrets.json
+# cat secrets.json
 
-echo "-----------"
-echo "serverless"
-cat serverless.yml
+# echo "-----------"
+# echo "serverless"
+# cat serverless.yml
